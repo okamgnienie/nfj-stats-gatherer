b6dd6214583229c004fbe6f4112e581462b088bf
Przemyslaw Hardyn <przemyslaw.hardyn.cv@gmail.com>
2021-12-15 09:22:51 +0100

    Add quartiles

--- gather_job_offer_stats.sh
+++ gather_job_offer_stats.sh
@@ -12,7 +12,7 @@ end=$'\e[0m'
 # Script parameters
 remote=false # true | false
 job_type="frontend" # frontend | backend | devops | fullstack | big-data | ai | testing
-current_step=1 # number 1 = 1000 PLN
+first_step=0 # number 1 = 1000 PLN
 last_step=50 # number 1 = 1000 PLN
 
 while getopts rj:f:t: flag
@@ -20,13 +20,14 @@ do
   case "${flag}" in
     r) remote=true;;
     j) job_type=${OPTARG};;
-    f) current_step=${OPTARG};;
+    f) first_step=${OPTARG};;
     t) last_step=${OPTARG};;
   esac
 done
 
 # Stats config
 step_size=1
+current_step=$first_step
 
 # Filename config
 report_filename_date=$(date +%m-%d-%Y_%H-%M-%S) # to easily distinguish the reports
@@ -49,11 +50,11 @@ printf "Report will be stored in ${red}${report_filename}${end}\n\n"
 
 # Helper functions
 get_job_type_stats_header() {
-  echo "\"${1} average\",\"${1} median\",\"${1} min\",\"${1} max\""
+  echo "\"${1} average\",\"${1} lower quartile\",\"${1} median\",\"${1} upper quartile\",\"${1} min\",\"${1} max\""
 }
 
 get_job_type_stats_markers() {
-  echo "@${1}_average@,@${1}_median@,@${1}_min@,@${1}_max@"
+  echo "@${1}_average@,@${1}_lower_quartile@,@${1}_median@,@${1}_upper_quartile@,@${1}_min@,@${1}_max@"
 }
 
 get_number_of_offers() {
@@ -78,21 +79,12 @@ print_num() {
   fi
 }
 
-get_median() {
+get_percentyle() {
   array_name=$1[@]
   array=("${!array_name}")
-  arr=($(printf '%d\n' "${array[@]}" | sort -n))
-  array_length=${#arr[@]}
+  percentile=$2
 
-  if (( $array_length % 2 == 1 )); then
-    val="${arr[ $(($array_length/2)) ]}"
-  else
-    (( j=array_length/2 ))
-    (( k=j-1 ))
-    (( val=(${arr[j]} + ${arr[k]})/2 ))
-  fi
-
-  echo $val
+  echo $(printf '%s\n' "${array[@]}" | sort -n | perl -e '$d='$percentile';@l=<>;print $l[int($d*$#l)]')
 }
 
 get_average() {
@@ -118,25 +110,33 @@ print_job_type_summary() {
 
   if [[ $array_length -gt 0 ]]; then
     average=$(get_average arr)
-    median=$(get_median arr)
+    lower_quartile=$(get_percentyle arr 0.25)
+    median=$(get_percentyle arr 0.5)
+    upper_quartile=$(get_percentyle arr 0.75)
     min=$(echo ${arr[0]})
     max=$(echo ${arr[${#arr[@]} - 1]})
 
     printf "${cyn}${seniority}${end}:\n"
     printf "%s%s\n" "average: " "${yel}${average} PLN${end}"
+    printf "%s%s\n" "lower quartile: " "${yel}${lower_quartile} PLN${end}"
     printf "%s%s\n" "median: " "${yel}${median} PLN${end}"
+    printf "%s%s\n" "upper quartile: " "${yel}${upper_quartile} PLN${end}"
     printf "%s%s\n" "min salary: " "${yel}${min} PLN${end}"
     printf "%s%s\n\n" "max salary: " "${yel}${max} PLN${end}"
   else
     average='-'
+    lower_quartile='-'
     median='-'
+    upper_quartile='-'
     min='-'
     max='-'
   fi
 
   # Store overall stats in the report
   sed -i '.bak' "s/@${seniority}_average@/${average}/" $report_filename
+  sed -i '.bak' "s/@${seniority}_lower_quartile@/${lower_quartile}/" $report_filename
   sed -i '.bak' "s/@${seniority}_median@/${median}/" $report_filename
+  sed -i '.bak' "s/@${seniority}_upper_quartile@/${upper_quartile}/" $report_filename
   sed -i '.bak' "s/@${seniority}_min@/${min}/" $report_filename
   sed -i '.bak' "s/@${seniority}_max@/${max}/" $report_filename
 }
@@ -158,11 +158,11 @@ for i in $(seq $current_step $(( last_step - 1 ))); do
 
   printf "%s%s%s%s%s%s%s%s%s%s%s%s%s\n" "Total offers with salary in range ${yel}${salary_from} PLN${end} - ${yel}${salary_to} PLN${end}: " "$(print_num $total)" " (trainee: " "$(print_num $trainee_offers)" ", junior: " "$(print_num $junior_offers)" ", mid: " "$(print_num $mid_offers)" ", senior: " "$(print_num $senior_offers)" ", expert: " "$(print_num $expert_offers)" ")"
 
-  line="${salary},${total},${trainee_offers},${junior_offers},${mid_offers},${senior_offers},${expert_offers}"
-  if [[ $current_step -eq 1 ]]; then
+  line="${salary_from},${total},${trainee_offers},${junior_offers},${mid_offers},${senior_offers},${expert_offers}"
+  if [[ $current_step -eq $first_step ]]; then
     line+=",$(get_job_type_stats_markers 'trainee'),$(get_job_type_stats_markers 'junior'),$(get_job_type_stats_markers 'mid'),$(get_job_type_stats_markers 'senior'),$(get_job_type_stats_markers 'expert')"
   else
-    line+=",,,,,,,,,,,,,,,,,,,,,"
+    line+=",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,"
   fi
 
   echo $line >> $report_filename
@@ -198,5 +198,5 @@ for i in $(seq $current_step $(( last_step - 1 ))); do
     print_job_type_summary all_expert_rates "expert"
   fi
 
-  current_step=$(( $current_step+$step_size ))
+  current_step=$(( $current_step + $step_size ))
 done
