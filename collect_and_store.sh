d2afb500efe5889c0f72111a699a306ddc2fd977
Przemyslaw Hardyn <przemyslaw.hardyn.cv@gmail.com>
2022-01-10 13:36:06 +0100

    Add script for collecting stats of interest

---
+++ collect_and_store.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Job types of interest
+job_types="frontend backend testing devops"
+todays_date=$(date +%m-%d-%Y)
+
+# Collect
+for job_type in $job_types; do
+  ./gather_job_offer_stats.sh -k -j $job_type -e b2b
+  ./gather_job_offer_stats.sh -k -j $job_type -e permanent
+  ./gather_job_offer_stats.sh -k -r -j $job_type -e b2b
+  ./gather_job_offer_stats.sh -k -r -j $job_type -e permanent
+done
+
+# Store generated reports in the repository
+git add ./reports
+git commit -m "Add reports from ${todays_date}"
+git push
